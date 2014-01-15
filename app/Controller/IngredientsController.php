0632bbc24634cf6cf15da85367a03ba0b320c0de
Constance Laborie <constance.laborie@gmail.com>
2014-01-15 14:36:59 +0100

    Ajout du popup de modif des plats

--- app/Controller/IngredientsController.php
+++ app/Controller/IngredientsController.php
@@ -20,7 +20,26 @@
             $this->Ingredient->create();
             if ($this->Ingredient->save($this->request->data)) {
                 $this->Session->setFlash(__('Votre ingredient a été sauvegardé.'));
+                echo "<script>openWin.close()</script>";
                 return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Impossible d\'ajouté un ingredient.'));
+        }
+}
+    public function add_popup() {
+        /*
+         * La variable $ingredients est envoyé à la vue menus/add
+         */
+         
+        $this->set('ingredients', $this->Ingredient->find('all')); // Initialise la variable ingredients à la requete find all
+        //$this->set('plats', $this->Menu->Plats->find('all'));
+        debug($this->request->data);
+        if ($this->request->is('post')) {
+            $this->Ingredient->create();
+            if ($this->Ingredient->save($this->request->data)) {
+                $this->Session->setFlash(__('Votre ingredient a été sauvegardé.'));
+                echo "<script>openWin.close()</script>";
+                //return $this->redirect(array('action' => 'index'));
             }
             $this->Session->setFlash(__('Impossible d\'ajouté un ingredient.'));
         }
